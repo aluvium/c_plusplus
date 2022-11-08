8244409deabf209082d833708e87ff59f130c64b
aluvium <aluvium@protonmail.com>
2022-11-08 12:31:01 +0100

    UP-2022-11-08

---
+++ files/13-dec-to-hexa.d/build/CMakeFiles/Dectohexa.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Dectohexa.dir/dectohexa.cxx.o"
+  "Dectohexa"
+  "Dectohexa.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Dectohexa.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ files/13-dec-to-hexa.d/build/DectohexaConfig.h
@@ -0,0 +1,2 @@
+#define Dectohexa_VERSION_MAJOR 1
+#define Dectohexa_VERSION_MINOR 0
